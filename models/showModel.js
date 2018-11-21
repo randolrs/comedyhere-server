f7c19d0f1e49ede943b8b22ac0b0513d6b1ad031
randolrs <r.shane.randolph@gmail.com>
2018-11-21 10:44:22 -0800

    update add show model

--- models/showModel.js
+++ models/showModel.js
@@ -2,6 +2,11 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 const showModel = new Schema({
     title: { type: String },
-    description: { type: String }
+    description: { type: String },
+    venueName: { type: String },
+    address: { type: String },
+    videoPreview: { type: String },
+    videoDescription: { type: String },
+    reasonRecommend: { type: String }
 })
 export default mongoose.model('shows', showModel)
